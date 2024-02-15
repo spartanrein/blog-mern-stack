4997da0751e2440cddad9894f2b6a3847d5cd1bf
Rein de Guzman <37677855+spartanrein@users.noreply.github.com>
2024-02-15 12:57:39 +0800

    create react app

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const password = encodeURIComponent("BcPIYH6kvuN799re")
 const uri = `mongodb+srv://test_user:${password}@blog.dysb6v1.mongodb.net/?retryWrites=true&w=majority`;
 const blogRoutes = require('./routes/blogRoutes')
+
 const db = mongoose.connect(uri)
 .then(result => {
     app.listen(port)
