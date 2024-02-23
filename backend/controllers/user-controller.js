3ce23d88da95d5aab2c07339a076e41668e57670
Rein de Guzman <37677855+spartanrein@users.noreply.github.com>
2024-02-23 12:33:30 +0800

    signup user method for usermodel

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -5,7 +5,14 @@ const login_user = async (req, res) => {
 }
 
 const signup_user = async (req, res) => {
-    res.json({"mssg": "Signed Up"})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.signup(email, password)
+        res.status(200).json({email, user})
+    } catch (err) {
+        res.status(400).json({error: err.message})
+    }
 }
 
-module.exports= { login_user, signup_user} 
+module.exports= { login_user, signup_user } 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
@@ -11,8 +12,16 @@ const userSchema = new Schema({
         type: String,
         required: true,
     } 
-},{timestamps:true})
+})
 
-const user = mongoose.model('User', userSchema)
-
-module.exports = user
+userSchema.statics.signup = async function(email, password){
+    const exists = await this.findOne({ email })
+    if (exists){
+        throw Error("Email already exists")
+    }
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+    const user = await this.create({ email, password: hash })
+    return user
+}
+module.exports = mongoose.model('User', userSchema)
